e2fb109b71bdc5a361c78eaaa8d825ecea4e0fb5
Tharun Gowda A V <120793029+TharunGowdaav@users.noreply.github.com>
2025-08-03 12:40:04 +0530

    Add files via upload

---
+++ src/ESP32_speech_to_text/Credentials.h
@@ -0,0 +1,19 @@
+// Provide all the crdentials here
+
+
+
+// Your WiFi Credential
+s 
+const char *ssid = "Tharungowdaav";       // WiFi SSID Name
+const char *password = "12121212";// WiFi Password ( Keep it blank if your WiFi router is open )
+
+
+// Parameters of Google Speech to Text API
+const String ApiKey = "wrgethethetehetWz3HkWVKC9cFcsmBMgw5BIxuc";// Google Speech to Text API key
+String SpeechtoText_Language = "en-IN"; // Language 
+
+// Parameters of OpenAI API 
+const char* chatgpt_token = "sk-ethrgerghewrgvbet4vw4ft4BlbkFJ0LmlAzbIV2y8eHbQbLVk"; // OpenAI API Key
+String OpenAI_Model = "gpt-3.5-turbo-instruct"; // Model
+String OpenAI_Temperature = "0.20"; // temperature
+String OpenAI_Max_Tokens = "40"; //Max Tokens 
---
+++ src/ESP32_speech_to_text/README.txt
@@ -0,0 +1,10 @@
+
+    This is the code for Speech to Text conversion and
+    Requesting OpenAi API to ask any questions.
+
+    To use this code succesfully:
+      1. You need to downgrade your ESP32 Boards Package to version 1.0.6.
+      2. Keep all the files attached with this zip in one single folder named ESP32_Speech_to_Text.
+      3. Add all your Credentials like WiFi SSID Name, Password, Google Cloud Key &
+         OpenAI API Key into Credentials.h Header File
+      4. Tested with Arduino IDE Verison 1.8.19
